e093b3236be20f47661d33a913043421efcfce92
JordiParraCrespo <jordiparra99@gmail.com>
2023-12-15 13:12:20 +0100

    fix: enable to run the package in expo go

--- src/ExpoCaptureModule.ts
+++ src/ExpoCaptureModule.ts
@@ -1,5 +1,35 @@
 import { requireNativeModule } from "expo-modules-core";
+import Constants, { ExecutionEnvironment } from "expo-constants";
 
-// It loads the native module object from the JSI or falls back to
-// the bridge module (from NativeModulesProxy) if the remote debugger is on.
-export default requireNativeModule("ExpoCapture");
+// `true` when running in Expo Go.
+const isExpoGo = Constants.executionEnvironment === ExecutionEnvironment.StoreClient;
+
+let ExpoCaptureModule;
+
+if (isExpoGo) {
+    {
+        /**
+         *  The ExpoCapture module is not available in the JSI environment. Are you trying to use it in the Expo Go app?
+         *  To use this module during development, you need to use a development build.  See https://docs.expo.dev/modules/use-standalone-expo-module-in-your-project/#4-test-the-published-module
+         */
+        ExpoCaptureModule = {
+            preventScreenCapture: async () => {},
+            allowScreenCapture: async () => {},
+            addListener: () => {},
+            removeListeners: () => {},
+        };
+    }
+} else {
+    ExpoCaptureModule = requireNativeModule("ExpoCapture");
+    // Fixes on Android:
+    // WARN  `new NativeEventEmitter()` was called with a non-null argument without the required `addListener` method.
+    if (!ExpoCaptureModule.addListener) {
+        ExpoCaptureModule.addListener = () => {};
+    }
+    // WARN  `new NativeEventEmitter()` was called with a non-null argument without the required `removeListeners` method.
+    if (!ExpoCaptureModule.removeListeners) {
+        ExpoCaptureModule.removeListeners = () => {};
+    }
+}
+
+export default ExpoCaptureModule;
